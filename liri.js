9367a4c06d752e42faf987a6d4b9be272349db77
Tamara Hall <tamarahall@Tamaras-MBP.lan>
2018-01-06 23:10:19 -0500

    spoitfy is pulling in artist name

--- liri.js
+++ liri.js
@@ -1,18 +1,22 @@
+// Pull in all of the required items for program to run
 require("dotenv").config();
 var keys= require("./keys");
 var twitter = require('twitter');
 var spotify = require('node-spotify-api');
 var fs= require("fs");
 
+// Set variables needed
 var spotify = new spotify(keys.spotify);
 var client = new twitter(keys.twitter);
 
+// Pull in the command & parameters
 var command = process.argv[2];
 var search = "";
 for ( i=3; i < process.argv.length; i++) {
     search += process.argv[i] + "+";
 }
-// NEED TO CHANGE THIS TO SWITCHES
+
+// Actions to take depending on the command & parameters given
 if (command === "my-tweets") {
     showTweet();
 } else if (command === "spotify-this-song") {
@@ -25,6 +29,7 @@ if (command === "my-tweets") {
     console.log("Not an approved command")
 }
 
+//  Twitter functions to run
 function showTweet() {
     console.log("Here are your lastest 20 Tweets \n");
     var params = {screen_name: "APKQueen00", count:20};
@@ -36,16 +41,20 @@ function showTweet() {
 });
 };
 
+//  Spotify functions to run
 function showSongs() {
+    if (!search) {
+        search = "happy";}
     spotify.search({ type: 'track', query: search }, function(err, data) {
-        if (err) {
-            return console.log('Error occurred: ' + err);
-          }
-        console.log(data.tracks);
+        if (err) throw err;
+        console.log("Here is the track information you need\n");
+        console.log("Artist: " + data.tracks.items[0].artists[0].name);
+        console.log("Song Name: " + data);
         });
     };
 
-function showMovies() {
+//  Movie functions to run
+    function showMovies() {
     console.log("Here is the movie information you need \n");
 };
 
